2e732f660105aa6631ecd97c90f0be544c5d01ab
raccoon2389 <roy0998@gmail.com>
2020-05-13 19:49:26 +0900

    .

---
+++ relu.py
@@ -0,0 +1,14 @@
+class RELU:
+    def __init__(self):
+        self.mask = None
+    def forward(self,x):
+        self.mask = (x>=0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+    def backward(self):
+        dout[self.mask]= 0
+        dx = dout
+
+        return dx
+        
