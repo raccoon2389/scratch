850f7d0dda1581c9e07acf6c3434999620d9fda4
raccoon2389 <roy0998@gmail.com>
2020-05-18 20:51:31 +0900

    .

--- prac2.py
+++ prac2.py
@@ -1,2 +1,12 @@
 import numpy as np
-x = np.array
+
+class Sigmoid :
+    def __init__(self):
+        self.out = None
+    def forward(self,x):
+        self.out = 1/(1+np.exp(-x))
+        return out
+    def backward(self,dout):
+        return dout*self.out*(1.0-self.out)
+
+class affaine22w
